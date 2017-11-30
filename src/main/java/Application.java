654352420666c1d2a369d9cc01e3d7817d1e89de
harinduliyanage <harindu.sul@gmail.com>
2017-11-29 16:51:23 -0800

    change group id and artifacId

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,7 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(value = {"main"})
 public class Application {
 
   public static void main(String[] args) {
