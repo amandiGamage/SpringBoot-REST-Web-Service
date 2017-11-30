8cbe7d35cd2a01440d0a0d5fb7af3003d02cddf8
harinduliyanage <harindu.sul@gmail.com>
2017-11-29 16:58:13 -0800

    Add SuperDAO interface to loosely coupling

---
+++ src/main/java/com/wearit/dao/SuperDAO.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wearit.dao;
+
+import java.util.List;
+
+
+public interface SuperDAO <T> {
+    public boolean add(T t) throws Exception;
+    public boolean update(T t) throws Exception;
+    public boolean delete(Integer id)throws Exception;
+    public T search(Integer id)throws Exception;
+    public List<T> getAll()throws Exception;
+}
